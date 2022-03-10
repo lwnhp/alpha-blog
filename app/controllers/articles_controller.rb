a751f05165afde04f701b1cb46a46d6e9a3a3cbc
lwnhp <b.straver@icloud.com>
2022-03-10 11:10:10 +0000

    actions toevoegen

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,32 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def index
+      @articles = Article.all 
+    end
+    
+    def new
+      @article = Article.new
+    end
+    
+    def edit
+        
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          flash[:notice] = "Article was created succesfully"
+          redirect_to @article
+        else
+          render 'new'
+        end
+    end
+    
+    def update
+        
+    end
+    
     def about
     end
 end
